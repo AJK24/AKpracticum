082c629864304752db7830e63b449ab4057723f7
Andrew <akv9509@saintjoe.edu>
2015-03-30 13:28:39 -0400

    Updated to include a change of counter.js

--- counter.js
+++ counter.js
@@ -1,10 +1,15 @@
-var stock = "1 lemon, 2 cabbages, and 101 eggs";
+var stock = "1 lemon, 2 cabbages, and 102 eggs";
+var big = "150 lemons, 3 bananas, and 500 peanuts";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
-  if (amount == 1) // only one left, remove the 's'
+  if (amount > 100){
+	amount = "zillions of";
+  }
+  else  if (amount == 1) // only one left, remove the 's'
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
     amount = "no";
   return amount + " " + unit;
 }
 print(stock.replace(/(\d+) (\w+)/g, minusOne));
+print(big.replace(/(\d+) (\w+)/g, minusOne));
